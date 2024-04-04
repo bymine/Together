68a68e4da040fae6a7ca8fad24cdb220248710df
Park Su Been <71866185+bymine@users.noreply.github.com>
2024-04-04 13:38:22 +0900

    프로젝트 초기 셋팅
    셋팅 작업
        1. eslint, prettier 설정
        2. lint-staged, husky hook 설정
        3. git commit message template 작성

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'no-var': 'error', // var 금지
+    'no-multiple-empty-lines': 'error', // 여러 줄 공백 금지
+    'no-console': ['error', { allow: ['warn', 'error', 'info'] }], // console.log() 금지
+    eqeqeq: 'error', // 일치 연산자 사용 필수
+    'dot-notation': 'error', // 가능하다면 dot notation 사용
+    'no-unused-vars': 'error', // 사용하지 않는 변수 금지
+  },
+};
