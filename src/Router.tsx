d60b420b9661bc8873b7688e8eb87cea410295a7
Park Su Been <71866185+bymine@users.noreply.github.com>
2024-04-06 14:34:29 +0900

    vercel.json 작성

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,8 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import AuthPage from './pages/Auth/AuthPage';
 import Dashboard from './pages/Dashboard/Dashboard';
@@ -9,6 +13,10 @@ const Router = () => {
     {
       element: <Layout />,
       children: [
+        {
+          path: '/',
+          element: <Navigate to="/auth" replace />,
+        },
         {
           path: '/auth',
           element: <AuthPage />,
